2290a14cf20fab07e4f57437b8a6d7ed23734258
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-12-08 19:17:16 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-    console.log(import.meta.env.VITE_BACKEND_URL);
-    
-
   return (
     <>
-    <h1>hi</h1>
       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
