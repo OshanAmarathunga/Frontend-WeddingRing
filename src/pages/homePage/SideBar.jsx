0f3ca924e49f5ac1a2a941d2c27a3834cd48cc24
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-12-21 23:06:30 +0530

    added card

--- src/pages/homePage/SideBar.jsx
+++ src/pages/homePage/SideBar.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import Checkbox from "@mui/material/Checkbox";
+import PersonCard from "../card/PersonCard";
 
 export default function SideBar() {
   return (
-    <div>
-      <Sidebar className="bg-pink-300"
-      >
-        <Menu>
-          <SubMenu label="I'm looking for" className="font-Roboto">
-            <MenuItem className="text-[12px]">
-              <Checkbox
-                sx={{
-                  color: "pink",
-                  "&.Mui-checked": {
+    <div className="flex">
+      <div>
+        <Sidebar className="bg-[#FFB37B]">
+          <Menu>
+            <SubMenu label="I'm looking for" className="font-Roboto">
+              <MenuItem className="text-[12px]">
+                <Checkbox
+                  sx={{
                     color: "pink",
-                  }, fontFamily: "Roboto",
-                  transform: "scale(0.7)"
-                }}
-                inputProps={{ "aria-label": "controlled" }}
-              />
-              Groom
-            </MenuItem>
-            <MenuItem className="text-[12px]">
-              <Checkbox
-                sx={{
-                  color: "pink",
-                  "&.Mui-checked": {
+                    "&.Mui-checked": {
+                      color: "pink",
+                    },
+                    fontFamily: "Roboto",
+                    transform: "scale(0.7)",
+                  }}
+                  inputProps={{ "aria-label": "controlled" }}
+                />
+                Groom
+              </MenuItem>
+              <MenuItem className="text-[12px]">
+                <Checkbox
+                  sx={{
                     color: "pink",
-                  },
-                  transform: "scale(0.7)",
-                  fontFamily: "Roboto",
-                  
-                }}
-                inputProps={{ "aria-label": "controlled" }}
-              />
-              Bride
-            </MenuItem>
-          </SubMenu>
-          <MenuItem> Documentation </MenuItem>
-          <MenuItem> Calendar </MenuItem>
-        </Menu>
-      </Sidebar>
+                    "&.Mui-checked": {
+                      color: "pink",
+                    },
+                    transform: "scale(0.7)",
+                    fontFamily: "Roboto",
+                  }}
+                  inputProps={{ "aria-label": "controlled" }}
+                />
+                Bride
+              </MenuItem>
+            </SubMenu>
+            <MenuItem> Documentation </MenuItem>
+            <MenuItem> Calendar </MenuItem>
+          </Menu>
+        </Sidebar>
+      </div>
+      <div className="p-7 flex">
+        <PersonCard />
+      </div>
     </div>
   );
 }
