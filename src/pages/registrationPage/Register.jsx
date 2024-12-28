38e2f219158c466ccae66154996bce1a127207d7
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-12-28 17:50:45 +0530

    bug fixed

--- src/pages/registrationPage/Register.jsx
+++ src/pages/registrationPage/Register.jsx
@@ -111,7 +111,7 @@ export default function Register() {
       <Backdrop
         sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
         open={open}
-        onClick={handleClose}
+        
       >
         <CircularProgress color="inherit" />
       </Backdrop>
